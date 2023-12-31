c74a7183963610d23495c851da0bd8d6aa91c3cc
mangano-ito <mangano-ito@hatena.ne.jp>
2023-12-31 19:47:30 +0900

    update word repository interface

--- shared/src/main/java/io/github/manganoito/wordman/shared/repository/DefaultWordRepository.kt
+++ shared/src/main/java/io/github/manganoito/wordman/shared/repository/DefaultWordRepository.kt
@@ -1,13 +1,14 @@
 package io.github.manganoito.wordman.shared.repository
 
+import io.github.manganoito.wordman.shared.model.Word
 import javax.inject.Inject
 
 class DefaultWordRepository @Inject constructor() : WordRepository {
-    override suspend fun addWord(word: String) {
+    override suspend fun addWord(word: Word) {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getAllWords(): List<String> {
+    override suspend fun getAllWords(): List<Word> {
         TODO("Not yet implemented")
     }
 }
