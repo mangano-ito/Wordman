bf5d786f353ca490f42706070e44312435804313
mangano-ito <mangano-ito@hatena.ne.jp>
2023-12-31 20:01:13 +0900

    setup hilt

--- mobile/src/main/java/io/github/manganoito/wordman/ui/screen/MainScreenViewModel.kt
+++ mobile/src/main/java/io/github/manganoito/wordman/ui/screen/MainScreenViewModel.kt
@@ -3,9 +3,11 @@ package io.github.manganoito.wordman.ui.screen
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.manganoito.wordman.shared.model.Word
 import javax.inject.Inject
 
+@HiltViewModel
 class MainScreenViewModel @Inject constructor(
     // TODO
 ) : ViewModel() {
