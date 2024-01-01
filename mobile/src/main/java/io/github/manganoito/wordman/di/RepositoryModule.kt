8dbb62f46333ba24e26f433ee23ed1c4273434b8
mangano-ito <mangano-ito@hatena.ne.jp>
2024-01-01 16:09:13 +0900

    explicitly specify dispatcher

--- mobile/src/main/java/io/github/manganoito/wordman/di/RepositoryModule.kt
+++ mobile/src/main/java/io/github/manganoito/wordman/di/RepositoryModule.kt
@@ -1,17 +1,24 @@
 package io.github.manganoito.wordman.di
 
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.github.manganoito.wordman.shared.data.WordDao
 import io.github.manganoito.wordman.shared.repository.DefaultWordRepository
 import io.github.manganoito.wordman.shared.repository.WordRepository
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RepositoryModule {
-    @Binds
+class RepositoryModule {
+    @Provides
     @Singleton
-    abstract fun bindWordRepository(impl: DefaultWordRepository): WordRepository
+    fun provideWordRepository(
+        wordDao: WordDao,
+    ): WordRepository = DefaultWordRepository(
+        wordDao = wordDao,
+        coroutineContext = Dispatchers.IO,
+    )
 }
--- shared/src/main/java/io/github/manganoito/wordman/shared/repository/DefaultWordRepository.kt
+++ shared/src/main/java/io/github/manganoito/wordman/shared/repository/DefaultWordRepository.kt
@@ -2,18 +2,22 @@ package io.github.manganoito.wordman.shared.repository
 
 import io.github.manganoito.wordman.shared.data.WordDao
 import io.github.manganoito.wordman.shared.model.Word
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 class DefaultWordRepository @Inject constructor(
     private val wordDao: WordDao,
+    private val coroutineContext: CoroutineContext = Dispatchers.IO,
 ) : WordRepository {
-    override suspend fun addWord(word: Word) {
+    override suspend fun addWord(word: Word): Unit = withContext(coroutineContext) {
         wordDao.insertWord(word)
     }
 
-    override suspend fun getAllWords(): List<Word> {
-        return wordDao.getAllWords()
+    override suspend fun getAllWords(): List<Word> = withContext(coroutineContext) {
+        wordDao.getAllWords()
     }
 
     override fun observeAllWords(): Flow<List<Word>> {
