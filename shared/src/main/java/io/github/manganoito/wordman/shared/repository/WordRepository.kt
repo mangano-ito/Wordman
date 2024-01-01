c3a8c5c74fa8a6210e109b717dd4b2267c26fbf7
mangano-ito <mangano-ito@hatena.ne.jp>
2024-01-01 15:38:37 +0900

    add addWord overload

--- shared/src/main/java/io/github/manganoito/wordman/shared/repository/WordRepository.kt
+++ shared/src/main/java/io/github/manganoito/wordman/shared/repository/WordRepository.kt
@@ -5,6 +5,18 @@ import kotlinx.coroutines.flow.Flow
 
 interface WordRepository {
     suspend fun addWord(word: Word)
+
+    suspend fun addWord(
+        word: String,
+        meaning: String,
+    ) = addWord(
+        word = Word(
+            id = 0,
+            value = word,
+            meaning = meaning,
+        ),
+    )
+
     suspend fun getAllWords(): List<Word>
 
     fun observeAllWords(): Flow<List<Word>>
