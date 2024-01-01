6e3df3388f8c8d207eaa587542ac1799ed23fbb0
mangano-ito <mangano-ito@hatena.ne.jp>
2024-01-01 15:49:53 +0900

    update layout

--- mobile/src/main/java/io/github/manganoito/wordman/ui/screen/AddWordScreen.kt
+++ mobile/src/main/java/io/github/manganoito/wordman/ui/screen/AddWordScreen.kt
@@ -1,6 +1,10 @@
 package io.github.manganoito.wordman.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -8,13 +12,15 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.manganoito.wordman.ui.theme.WordManTheme
 
 @Composable
@@ -56,13 +62,29 @@ private fun AddWordScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun AddWordScreenContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        TextField(
+    Column(
+        modifier = modifier.padding(
+            horizontal = 16.dp,
+        ),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        OutlinedTextField(
             value = "",
             onValueChange = {},
+            modifier = Modifier.fillMaxWidth(),
             label = { Text(text = "Word") },
         )
-        Button(onClick = { /*TODO*/ }) {
+        OutlinedTextField(
+            value = "",
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = {},
+            label = { Text(text = "Meaning") },
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.End),
+        ) {
             Text(text = "Add")
         }
     }
