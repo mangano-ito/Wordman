a03b6c3b8d093833bb6c0c818336611565f811aa
mangano-ito <mangano-ito@hatena.ne.jp>
2024-01-01 15:24:17 +0900

    add word list stream

--- shared/src/main/java/io/github/manganoito/wordman/shared/repository/WordRepository.kt
+++ shared/src/main/java/io/github/manganoito/wordman/shared/repository/WordRepository.kt
@@ -1,8 +1,11 @@
 package io.github.manganoito.wordman.shared.repository
 
 import io.github.manganoito.wordman.shared.model.Word
+import kotlinx.coroutines.flow.Flow
 
 interface WordRepository {
     suspend fun addWord(word: Word)
     suspend fun getAllWords(): List<Word>
+
+    fun observeAllWords(): Flow<List<Word>>
 }
