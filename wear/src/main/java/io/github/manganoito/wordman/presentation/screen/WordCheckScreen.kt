3f3374a4919affe89dbb71cd05909dc42d1e8c4c
mangano-ito <mangano-ito@hatena.ne.jp>
2024-01-03 21:43:11 +0900

    use actual state instead of hardcoded values

--- wear/src/main/java/io/github/manganoito/wordman/presentation/screen/WordCheckScreen.kt
+++ wear/src/main/java/io/github/manganoito/wordman/presentation/screen/WordCheckScreen.kt
@@ -5,16 +5,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.CircularProgressIndicator
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.ScalingLazyColumn
 import androidx.wear.compose.material.Text
 import io.github.manganoito.wordman.presentation.theme.WordManPreviewTheme
+import io.github.manganoito.wordman.shared.model.Word
 
 @Composable
 internal fun WordCheckScreen(
@@ -23,24 +28,51 @@ internal fun WordCheckScreen(
     LaunchedEffect(Unit) {
         viewModel.doSync()
     }
-    WordCheckScreen()
+    WordCheckScreen(
+        state = viewModel.state,
+    )
 }
 
 @Composable
-private fun WordCheckScreen() {
+private fun WordCheckScreen(
+    state: WordCheckScreenUiState,
+) {
+    when (state) {
+        is WordCheckScreenUiState.Loading -> {
+            CircularProgressIndicator()
+        }
+
+        is WordCheckScreenUiState.Loaded -> {
+            WordCheckScreenContent(
+                words = state.words,
+            )
+        }
+    }
+}
+
+@Composable
+private fun WordCheckScreenContent(
+    words: List<Word>,
+    modifier: Modifier = Modifier,
+) {
+    val answers by remember(words) {
+        mutableStateOf(
+            words.map { it.meaning }.shuffled(),
+        )
+    }
     ScalingLazyColumn(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         item {
             WordCheckHeader(
-                word = "Horologist",
+                word = words.first().value,
             )
         }
         item {
             WordCheckAnswerList(
-                answers = listOf("時計学者、時計工", "時計学者、時計工", "時計学者、時計工"),
+                answers = answers,
                 onAnswer = { _ -> },
             )
         }
@@ -91,5 +123,20 @@ private fun WordCheckAnswerList(
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
 private fun WordCheckScreenPreview() = WordManPreviewTheme {
-    WordCheckScreen()
+    WordCheckScreen(
+        state = WordCheckScreenUiState.Loaded(
+            words = listOf(
+                Word(
+                    id = 1,
+                    value = "Word",
+                    meaning = "語，単語",
+                ),
+                Word(
+                    id = 2,
+                    value = "Phrase",
+                    meaning = "成句，熟語，慣用句，決まり文句",
+                ),
+            ),
+        ),
+    )
 }
