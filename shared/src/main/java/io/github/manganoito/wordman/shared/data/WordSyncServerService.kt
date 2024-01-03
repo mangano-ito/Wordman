d97bb5659d34b1616931b806c5d76817343628bc
mangano-ito <mangano-ito@hatena.ne.jp>
2024-01-03 18:28:07 +0900

    implement getRandomWord

--- shared/src/main/java/io/github/manganoito/wordman/shared/data/WordSyncServerService.kt
+++ shared/src/main/java/io/github/manganoito/wordman/shared/data/WordSyncServerService.kt
@@ -4,21 +4,33 @@ import com.google.protobuf.Empty
 import com.google.protobuf.Timestamp
 import io.github.manganoito.wordman.shared.data.proto.WordSyncProtoData.WordData
 import io.github.manganoito.wordman.shared.data.proto.WordSyncServerServiceGrpcKt
+import io.github.manganoito.wordman.shared.model.Word
 import io.github.manganoito.wordman.shared.repository.WordRepository
 
 class WordSyncServerService(
     private val wordRepository: WordRepository,
 ) : WordSyncServerServiceGrpcKt.WordSyncServerServiceCoroutineImplBase() {
     override suspend fun getRandomWord(request: Empty): WordData {
-        return WordData.newBuilder()
-            .setValue("test")
-            .setMeaning("〔機器や製法などの〕検査、試験運転、動作確認")
-            .setUpdated(
-                System.currentTimeMillis().toProtoTimestamp(),
-            )
-            .build()
+        val word = wordRepository.getSomeRandomWords(1).firstOrNull()
+        return word?.toWordData() ?: emptyWordData
     }
 }
+
+private val emptyWordData = WordData.newBuilder()
+    .setValue("test")
+    .setMeaning("〔機器や製法などの〕検査、試験運転、動作確認")
+    .setUpdated(
+        System.currentTimeMillis().toProtoTimestamp(),
+    )
+    .build()
+
+private fun Word.toWordData(): WordData = WordData.newBuilder()
+    .setValue(value)
+    .setMeaning(meaning)
+    .setUpdated(
+        System.currentTimeMillis().toProtoTimestamp(),
+    )
+    .build()
 
 private fun Long.toProtoTimestamp(): Timestamp = Timestamp.newBuilder()
     .setSeconds(this / 1000)
