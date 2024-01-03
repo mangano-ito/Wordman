de974a7c275db718246205eb6e843d4aceb34666
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2024-01-03 22:34:29 +0900

    update word repository interface

--- shared/src/main/java/io/github/manganoito/wordman/shared/repository/WordRepository.kt
+++ shared/src/main/java/io/github/manganoito/wordman/shared/repository/WordRepository.kt
@@ -1,6 +1,8 @@
 package io.github.manganoito.wordman.shared.repository
 
+import io.github.manganoito.wordman.shared.model.Word
+
 interface WordRepository {
-    suspend fun addWord(word: String)
-    suspend fun getAllWords(): List<String>
+    suspend fun addWord(word: Word)
+    suspend fun getAllWords(): List<Word>
 }
