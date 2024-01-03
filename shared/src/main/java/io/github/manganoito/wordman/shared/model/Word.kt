23c76071fd8507225bee0002617c131439339b76
mangano-ito <mangano-ito@hatena.ne.jp>
2024-01-03 14:02:05 +0900

    auto generate id

--- shared/src/main/java/io/github/manganoito/wordman/shared/model/Word.kt
+++ shared/src/main/java/io/github/manganoito/wordman/shared/model/Word.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Word(
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val value: String,
     val meaning: String,
