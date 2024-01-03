0df1f2a1938509893359b73442a0dca56191c109
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2024-01-03 22:34:46 +0900

    add remote activity call when a new word is added

--- mobile/src/main/java/io/github/manganoito/wordman/ui/screen/AddWordScreenViewModel.kt
+++ mobile/src/main/java/io/github/manganoito/wordman/ui/screen/AddWordScreenViewModel.kt
@@ -1,18 +1,29 @@
 package io.github.manganoito.wordman.ui.screen
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.data.WearDataLayerRegistry
+import com.google.android.horologist.data.activityConfig
+import com.google.android.horologist.datalayer.phone.PhoneDataLayerAppHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.manganoito.wordman.di.GlobalScope
 import io.github.manganoito.wordman.shared.repository.WordRepository
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
+@OptIn(ExperimentalHorologistApi::class)
 @HiltViewModel
 class AddWordScreenViewModel @Inject constructor(
     private val wordRepository: WordRepository,
+    private val dataLayerAppHelper: PhoneDataLayerAppHelper,
+    private val wearDataLayerRegistry: WearDataLayerRegistry,
+    @GlobalScope private val globalScope: CoroutineScope,
 ) : ViewModel() {
     var state by mutableStateOf(AddWordScreenUiState())
         private set
@@ -30,11 +41,22 @@ class AddWordScreenViewModel @Inject constructor(
     }
 
     fun commitSave() {
-        viewModelScope.launch {
+        globalScope.launch {
             wordRepository.addWord(
                 word = state.word,
                 meaning = state.meaning,
             )
+            val nodes = wearDataLayerRegistry.nodeClient.connectedNodes.await()
+            val config = activityConfig {
+                classFullName = "io.github.manganoito.wordman.presentation.WordRememberedActivity"
+            }
+            nodes.forEach {
+                val result = dataLayerAppHelper.startRemoteActivity(
+                    node = it.id,
+                    config = config,
+                )
+                Log.d("WordMan", "startRemoteActivity result: $result")
+            }
         }
     }
 }
