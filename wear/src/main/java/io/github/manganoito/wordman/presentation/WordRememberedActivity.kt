cc1de4ea196b3c1f10fb5a4a702a3e86827c6498
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2024-01-03 22:34:46 +0900

    add a new word remembered screen

---
+++ wear/src/main/java/io/github/manganoito/wordman/presentation/WordRememberedActivity.kt
@@ -0,0 +1,35 @@
+package io.github.manganoito.wordman.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.wear.compose.material.MaterialTheme
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.manganoito.wordman.presentation.screen.WordRememberedScreen
+import io.github.manganoito.wordman.presentation.theme.WordManTheme
+
+@AndroidEntryPoint
+class WordRememberedActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WordManTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    WordRememberedScreen(
+                        onFinish = { finish() },
+                    )
+                }
+            }
+        }
+    }
+}
