a6f08f7349462aeecdfd4e368ef6ac35a7a0f134
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2024-01-03 22:34:33 +0900

    add database class

---
+++ shared/src/main/java/io/github/manganoito/wordman/shared/data/WordManDatabase.kt
@@ -0,0 +1,33 @@
+package io.github.manganoito.wordman.shared.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import io.github.manganoito.wordman.shared.model.Word
+
+@Database(
+    entities = [
+        Word::class,
+    ],
+    version = 1,
+)
+abstract class WordManDatabase : RoomDatabase() {
+    companion object {
+        const val DATABASE_NAME = "wordman.db"
+
+        @Volatile
+        private var instance: WordManDatabase? = null
+
+        fun getInstance(context: Context): WordManDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): WordManDatabase {
+            return Room.databaseBuilder(context, WordManDatabase::class.java, DATABASE_NAME)
+                .build()
+        }
+    }
+}
