0370198779e463a4cc2338653e0f9ef5d3c27226
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2024-01-11 01:13:58 +0900

    add answer dialog components

---
+++ wear/src/main/java/io/github/manganoito/wordman/presentation/component/WrongAnswerDialog.kt
@@ -0,0 +1,78 @@
+package io.github.manganoito.wordman.presentation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.dialog.Confirmation
+import androidx.wear.compose.material.dialog.Dialog
+import io.github.manganoito.wordman.presentation.screen.WordRememberedScreenUiState
+import io.github.manganoito.wordman.presentation.theme.WordManPreviewTheme
+
+@Composable
+fun WrongAnswerDialog(
+    onFinish: () -> Unit,
+) {
+    var showDialog by rememberSaveable { mutableStateOf(true) }
+    Dialog(
+        showDialog = showDialog,
+        onDismissRequest = {
+            showDialog = false
+            onFinish()
+        },
+    ) {
+        Confirmation(
+            onTimeout = {
+                showDialog = false
+                onFinish()
+            },
+        ) {
+            WrongAnswerNotice()
+        }
+    }
+}
+
+@Composable
+fun WrongAnswerNotice(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        Icon(
+            imageVector = Icons.Default.Cancel,
+            contentDescription = null,
+            modifier = Modifier.size(48.dp),
+            tint = Color.Red,
+        )
+        Text(
+            text = "Nah, wrong answer...\nTry again!",
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+private fun WrongAnswerNoticePreview() = WordManPreviewTheme {
+    WrongAnswerNotice()
+}
